c94c63cda25b33f24af60631fdbc7bbdad3081b4
Kevin_Yang_tw <kevinyaungputra@gmail.com>
2026-08-10 23:17:05 +0800

    fix: retain shared build manifest

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    emptyOutDir: false,
+  },
   plugins: [reactRouter(), cloudflare({ configPath: "./wrangler.base.jsonc" })],
 });
